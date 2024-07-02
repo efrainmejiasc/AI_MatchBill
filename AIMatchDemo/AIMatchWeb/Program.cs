2f2bb892278fc7b7a0c6d0f938367e613d3403a2
efrainmejiasc <efrain.mejias@vettica.co>
2024-07-02 09:07:35 -0400

    update

---
+++ AIMatchDemo/AIMatchWeb/Program.cs
@@ -0,0 +1,10 @@
+using AIMatchWeb;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration, builder.Environment);
+startup.ConfigureServices(builder.Services);
+var app = builder.Build();
+startup.Configure(app, app.Environment);
+
+app.Run();
---
+++ AIMatchDemo/AIMatchWeb/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+namespace AIMatchWeb
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
+        {
+            Configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //**************************************************************************
+
+            //services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews();
+            services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(20);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseExceptionHandler("/Error");
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            // app.UseAuthorization();
+
+            app.UseSession();
+            app.UseEndpoints(endpoints =>
+            {
+                // endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller}/{action}/{id?}",
+                defaults: new { controller = "Home", action = "Index" });
+            });
+
+        }
+
+
+    }
+}
